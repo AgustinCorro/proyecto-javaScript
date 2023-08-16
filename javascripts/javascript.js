b29374d02e9a318d52b264e0f13440b77d49d0bd
Agustin Corro <corroagus@hotmail.com>
2023-08-16 13:24:48 -0300

    segunda pre entrega

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,3 +1,5 @@
+/*Primera pre-entrega
+
 alert("Bienvenido a la aventura")
 let nombre = prompt("Dime tu nombre")
 alert(`¡Hola ${nombre} bienvenido/a!`)
@@ -34,7 +36,50 @@ function camino(){
 if (eleccion == "derecha"){
     camino()
 }
+*/
 
-alert("¡Muchas gracias por participar de la aventura!")
-    
+
+
+
+//Segunda pre-entrega
+
+const notas = [
+    {nota : "10"},
+    {nota : "9"},
+    {nota : "8"},
+    {nota : "7"},
+    {nota : "6"},
+    {nota : "5"},
+    {nota : "4"},
+    {nota : "3"},
+    {nota : "2"},
+    {nota : "1"},
+    {nota : "0"},
+]
+
+const notaIngresada = prompt("Ingrese su nota :")
+
+const notaComprobada = notas.find((notaObj => notaObj.nota === notaIngresada ));
+
+while (notaComprobada){
+    if (notaComprobada.nota === "0"){
+        alert(`Tu examen esta aplazado`)
+        break;
+    }else if(notaComprobada.nota >=  7){
+        alert(`Tu examen esta aprobado`)
+        break;
+    }else if(notaComprobada.nota === "10"){
+        alert(`Tu examen esta aprobado con la mejor nota`)
+        break;
+    }else if(notaComprobada.nota <= "6"){
+        alert(`Tu examen esta desaprobado`)
+        break;
+    }
+}if (notaComprobada === false)
+alert(prompt("Nota no valida por favor ingrese su nota en valor numerico"))
+
+
+
+
+
 
