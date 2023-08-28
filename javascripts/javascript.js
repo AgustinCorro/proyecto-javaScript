536508e14a7a1ae2180c865635a2e128303856de
Agustin Corro <corroagus@hotmail.com>
2023-08-28 14:23:45 -0300

    Alargando codigo para la segunda pre-entrega

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,40 +1,69 @@
-const notas = [
-    {nota : "10"},
-    {nota : "9"},
-    {nota : "8"},
-    {nota : "7"},
-    {nota : "6"},
-    {nota : "5"},
-    {nota : "4"},
-    {nota : "3"},
-    {nota : "2"},
-    {nota : "1"},
-    {nota : "0"},
-]
-
-const notaIngresada = prompt("Ingrese su nota :")
-
-const notaComprobada = notas.find((notaObj => notaObj.nota === notaIngresada ));
-
-while (notaComprobada){
-    if (notaComprobada.nota === "0"){
-        alert(`Tu examen esta aplazado`)
-        break;
-    }else if(notaComprobada.nota >=  7){
-        alert(`Tu examen esta aprobado`)
-        break;
-    }else if(notaComprobada.nota === "10"){
-        alert(`Tu examen esta aprobado con la mejor nota`)
-        break;
-    }else if(notaComprobada.nota <= "6"){
-        alert(`Tu examen esta desaprobado`)
-        break;
+class Alumno {
+    constructor() {
+        this.calificaciones = [];
     }
-}if (notaComprobada === false)
-alert(prompt("Nota no valida por favor ingrese su nota en valor numerico"))
 
+    registrarNota(nota) {
+        if (this.calificaciones.length < 10) { 
+            this.calificaciones.push(nota);
+            return true;
+        }
+        return false;
+    }
 
+    calcularAprobadas() {
+        return this.calificaciones.filter(nota => nota >= 7).length;
+    }
 
+    calcularDesaprobadas() {
+        return this.calificaciones.filter(nota => nota < 7).length;
+    }
+
+    verificarNota(nota) {
+        if (typeof nota === 'number' && nota >= 0 && nota <= 10) {
+            if (nota === 0) {
+                return "aplazado";
+            } else if (nota >= 7) {
+                return "aprobado";
+            } else if (nota === 10) {
+                return "aprobado con la mejor nota";
+            } else {
+                return "desaprobado";
+            }
+        } else {
+            return "nota no válida";
+        }
+    }
+}
+
+const alumno = new Alumno();
+const notas = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+let notasIngresadas = 0; 
+
+while (notasIngresadas < 10) { 
+    const notaIngresada = parseFloat(prompt("Ingrese su nota:"));
+
+    if (!isNaN(notaIngresada)) {
+        const verificacion = alumno.verificarNota(notaIngresada);
+
+        if (verificacion !== "nota no válida") {
+            if (alumno.registrarNota(notaIngresada)) {
+                notasIngresadas++;
+                alert(`Tu examen está ${verificacion}`);
+                const aprobadas = alumno.calcularAprobadas();
+                const desaprobadas = alumno.calcularDesaprobadas();
+                alert(`Hasta ahora, has aprobado ${aprobadas} materias y desaprobado ${desaprobadas} materias.`);
+            } else {
+                alert("Ya has ingresado el máximo de 10 notas.");
+            }
+        } else {
+            alert("Nota no válida. Por favor, ingrese su nota en valor numérico.");
+        }
+    } else {
+        alert("Ingreso inválido. Por favor, ingrese un número.");
+    }
+}
 
 
 
